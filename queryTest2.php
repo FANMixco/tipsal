cea58f7e2e6b2a282e18a4ac6c8b2a1b73b2ade0
Federico Navarrete <fanmixco@gmail.com>
2013-01-29 18:39:26 -0600

    Actualización Azure
    Conexiones

--- queryTest2.php
+++ queryTest2.php
@@ -2,7 +2,7 @@
 	require_once 'config.inc.php';
 	require_once 'Database.class.php';
 	$sql	= "SELECT idType as 'key', type as title, verticalImage, horizontalImage, description FROM types ";
-	$db = new Database('localhost', 'root', '', 'tipsal');
+	$db = new Database('us-cdbr-azure-east-b.cloudapp.net', 'b396c404e52f12', '8c6a2a67', 'tipsaldb');
 	$db->connect();
 	echo $db->query2JSON($sql);
 	$db->close();
--- queryTest3.php
+++ queryTest3.php
@@ -2,7 +2,7 @@
 	require_once 'config.inc.php';
 	require_once 'Database.class.php';
 	$sql	= "SELECT CASE (idType) WHEN 'group1' THEN 0 WHEN 'group2' THEN 1 WHEN 'group3' THEN 2 END AS 'group', dish as title, description, ingredients, procedures, backgroundImage, idDish AS gallery, (SELECT ROUND(AVG(rate),1) FROM rates WHERE idDish=dishes.idDish) AS rate FROM dishes order by rate desc limit 5";
-	$db = new Database('localhost', 'root', '', 'tipsal');
+	$db = new Database('us-cdbr-azure-east-b.cloudapp.net', 'b396c404e52f12', '8c6a2a67', 'tipsaldb');
 	$db->connect();
 	echo $db->query2JSON($sql);
 	$db->close();
